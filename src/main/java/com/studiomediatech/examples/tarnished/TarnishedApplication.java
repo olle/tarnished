ca7b4664c683bbeeddd1baa8d0baa575505f4988
Olle Törnström <toernstroem@synyx.de>
2020-12-22 20:09:12 +0100

    Initial commit

---
+++ src/main/java/com/studiomediatech/examples/tarnished/TarnishedApplication.java
@@ -0,0 +1,13 @@
+package com.studiomediatech.examples.tarnished;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarnishedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarnishedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studiomediatech/examples/tarnished/TarnishedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiomediatech.examples.tarnished;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarnishedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
